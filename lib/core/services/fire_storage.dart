1ab6eb92d73332fd25d1954419b618ece92807a0
mohamedrafat <mtag66870@gmail.com>
2026-03-29 23:05:07 +0200

    add storage service and fire storage
    create storage service to be mainly any database service can extends it and work easly and now i using firebase storage database

---
+++ lib/core/services/fire_storage.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:fruits_hub_dashboard/core/services/storage_service.dart';
+import 'package:path/path.dart' as p;
+class FireStorage implements StorageService {
+  final storageRef = FirebaseStorage.instance.ref();
+
+  @override
+  Future<String> uploadImage(File image, String path) async {
+    final fileName = p.basename(image.path);  // image name + extension mango.png
+    final imageRef = storageRef.child('$path/$fileName');
+
+    await imageRef.putFile(image);
+
+    return await imageRef.getDownloadURL();
+  }
+}
